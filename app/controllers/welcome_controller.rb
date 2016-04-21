cc7fce6e4088d9132623cfa8b29a41ca3860d963
dannyvassallo <danielvassallo87@gmail.com>
2016-04-21 15:46:29 -0400

    testing xframe options from controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    response.headers.delete "X-Frame-Options"
     @rd_rules = RulesFile.where(station: "Radio Disney").to_a
     @rdc_rules = RulesFile.where(station: "Radio Disney Country").to_a
     if current_user != nil && current_user.role == 'admin'
